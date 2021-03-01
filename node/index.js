295c273fb76abe9f84e391797068e5b3f07d351c
Kelvyn Santana <kelvyn.santana@bbnk.com.br>
2021-03-01 00:13:46 -0300

    add docker compose

--- node/index.js
+++ node/index.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const express = require("express");
+const mysql = require("mysql");
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
 
-app.get('/',(req, res) => {
+const connection = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) values('Kelvyn')`;
+
+connection.query(sql);
+connection.end();
+
+app.get("/", (req, res) => {
   res.json({
-    ok: true
-  })
-})
+    ok: true,
+  });
+});
 
 app.listen(port, () => {
-  console.log("Running in port " + port)
-})
+  console.log("Running in port " + port);
+});
